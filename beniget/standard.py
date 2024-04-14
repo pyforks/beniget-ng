1bce48d9b652b045c0c4d57730f898395d4c4c36
tristanlatr <19967168+tristanlatr@users.noreply.github.com>
2024-04-14 11:50:30 -0400

    Support type parameters (#5)

--- beniget/standard.py
+++ beniget/standard.py
@@ -7,40 +7,7 @@ from beniget import beniget, Ancestors
 
 __all__ = ('Ancestors', 'Def', 'DefUseChains', 'UseDefChains')
             
-class _CollectFutureImports(beniget._CollectFutureImports):
-    def visit_Str(self, node):
-        pass
-
-class Def(beniget.Def):
-    def name(self):
-        if isinstance(self.node, ast.arg):
-            return self.node.arg
-        elif isinstance(self.node, ast.ExceptHandler) and self.node.name:
-            return self.node.name
-        return super().name()
-
 class DefUseChains(beniget.DefUseChains):
-
-    def __init__(self, *args, **kw):
-        self._init_args = (args, kw)
-
-    def visit_Module(self, node):
-        # Patch beniget
-        (_oldCollec, 
-         beniget._CollectFutureImports) = (beniget._CollectFutureImports, 
-                                           _CollectFutureImports)
-        (_oldDef, beniget.Def) = (beniget.Def, Def)
-        
-        # defer the __init__ after patching Def
-        args, kw = self._init_args
-        super().__init__(*args, **kw)
-
-        try:
-            return super().visit_Module(node)
-        finally:
-            # Unpatch
-            beniget.Def = _oldDef
-            beniget._CollectFutureImports = _oldCollec
     
     def visit_skip_annotation(self, node):
         if isinstance(node, ast.arg):
@@ -52,14 +19,14 @@ class DefUseChains(beniget.DefUseChains):
             # standard library nodes does not wrap 
             # the exception 'as' name in Name instance, so we use
             # the ExceptHandler instance as reference point.
-            dnode = self.chains.setdefault(node, Def(node))
+            dnode = self.chains.setdefault(node, beniget.Def(node))
             self.set_definition(node.name, dnode)
             if dnode not in self.locals[self._scopes[-1]]:
                 self.locals[self._scopes[-1]].append(dnode)
         self.generic_visit(node)
     
     def visit_arg(self, node, skip_annotation=False):
-        dnode = self.chains.setdefault(node, Def(node))
+        dnode = self.chains.setdefault(node, beniget.Def(node))
         self.set_definition(node.arg, dnode)
         if dnode not in self.locals[self._scopes[-1]]:
             self.locals[self._scopes[-1]].append(dnode)
@@ -68,7 +35,7 @@ class DefUseChains(beniget.DefUseChains):
         return dnode
     
     def visit_ExtSlice(self, node):
-        dnode = self.chains.setdefault(node, Def(node))
+        dnode = self.chains.setdefault(node, beniget.Def(node))
         for elt in node.dims:
             self.visit(elt).add_user(dnode)
         return dnode
@@ -80,11 +47,11 @@ class DefUseChains(beniget.DefUseChains):
     visit_NameConstant = visit_Num = visit_Str = \
         visit_Bytes = visit_Ellipsis = visit_Constant = beniget.DefUseChains.visit_Constant
 
-class UseDefChains(object):
+class UseDefChains(beniget.UseDefChains):
     def __init__(self, defuses):
         self.chains = {}
         for chain in defuses.chains.values():
-            if isinstance(chain.node, (ast.Name, ast.arg)):
+            if isinstance(chain.node, (ast.Name, ast.arg)): # the only change is here
                 self.chains.setdefault(chain.node, [])
             for use in chain.users():
                 self.chains.setdefault(use.node, []).append(chain)
